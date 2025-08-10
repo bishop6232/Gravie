82e4ba7895fd650dd0dc2f54ed4539416f4e7192
Maduakolam uzoma <munachimsouzoma814@gmail.com>
2025-08-10 10:55:57 +0200

    added the character
    Implemented the character movement logic, add ground animations to the character

---
+++ Gravie_game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 moveInput;
+    Rigidbody2D rb;
+
+    Animator animator;
+    public float walkspeed = 3f;
+    public float runspeed = 7f;
+
+    private bool isMoving = false;
+    private bool isRunning = false;
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (isMoving)
+            {
+                if (isRunning)
+                {
+                    return runspeed;
+                }
+                else
+                {
+                    return walkspeed;
+                }
+
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    public bool IsMoving
+    {
+        get
+        {
+            return isMoving;
+        }
+        private set
+        {
+            isMoving = value;
+            animator.SetBool("isMoving", isMoving);
+
+        }
+    }
+
+    public bool IsRunning
+    {
+        get
+        {
+            return isRunning;
+        }
+        private set
+        {
+            isRunning = value;
+            animator.SetBool("isRunning", isRunning);
+        }
+    }
+    public bool isFacingRight = true; // default to facing right
+  
+    public bool IsFacingRight
+    {
+        get
+        {
+            return isFacingRight;
+        }
+        private set
+        {
+            if (isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            isFacingRight = value;
+        }
+    }
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.linearVelocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true; // Facing right
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false; // Facing left
+        }
+      
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+
+        }
+    }
+}
