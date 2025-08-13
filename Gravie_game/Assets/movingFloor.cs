d6c4b3faf7c0342c7de5e165def241992b725fe2
Elif Demir <elifsimaydemir@hotmail.com>
2025-08-13 22:03:20 +0200

    updated the background
    added some more details
    added animation for the blade and some platforms to make it a little bit difficult

---
+++ Gravie_game/Assets/movingFloor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class movingFloor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed; //speed of the moving floor
+    public int startingPoint; //starting point index
+    public Transform[] points; //array of points for the floor to move between
+
+    private int i; //index of the array
+
+    void Start()
+    {
+        transform.position = points[startingPoint].position; //set the initial position of the floor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checking the distance of the platform and the point
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++; //increase the index
+            if (i >= points.Length) //check if the platform was on the last point after the increase
+            {
+                i = 0; //reset the index
+            }
+        }
+
+        //moving the platform to the point position with the index "i"
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+}
